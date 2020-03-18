8953904d903080d1fc0b60262491ed5ede246913
TimmyGOGO <temich941@mail.ru>
2020-03-19 00:32:50 +0300

    Second lesson

--- script.js
+++ script.js
@@ -7,23 +7,47 @@ const formSearch = document.querySelector('.form-search'),
   dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to'),
   inputDateDepart = formSearch.querySelector('.input__date-depart');
 
-// бд городов:
-const city = ['Москва', 'Санкт-Петербург', 'Тверь', 'Минск'];
+// константы для подгрузки данных:
+const CITY_API = 'http://api.travelpayouts.com/data/ru/cities.json',
+  citiesLocalBD = 'dataBase/cities.json',
+  proxy = 'https://cors-anywhere.herokuapp.com/',
+  API_KEY = '1ed3ba2e1f02eb124524399a74893911',
+  calendar = 'http://min-prices.aviasales.ru/calendar_preload';
+
+// const city = ['Москва', 'Санкт-Петербург', 'Тверь', 'Минск'];
+let city = [];
 
 // функции:
+
+const getData = (url, callback) => {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', url);
+  request.addEventListener('readystatechange', (event) => {
+    if (request.readyState !== 4) return;
+
+    if (request.status === 200) {
+      callback(request.response);
+    } else {
+      console.error(request.status);
+    }
+  });
+  request.send();
+};
+
 const showCity = (input, list) => () => {
   list.textContent = '';
 
   if (input.value !== '') {
     const filterCity = city.filter((item) => {
-      const fixItem = item.toLowerCase();
+      const fixItem = item.name.toLowerCase();
       return fixItem.includes(input.value.toLowerCase());
     });
 
     filterCity.forEach((item) => {
       const li = document.createElement('li');
       li.classList.add('dropdown__city');
-      li.textContent = item;
+      li.textContent = item.name;
       list.append(li);
       console.log(li);
     });
@@ -58,3 +82,10 @@ dropdownCitiesTo.addEventListener(
   'click',
   selectCity(inputCitiesTo, dropdownCitiesTo)
 );
+
+// вызовы функций
+
+// getData(citiesLocalBD, (data) => {
+getData(proxy + CITY_API, (data) => {
+  city = JSON.parse(data).filter((item) => item.name);
+});
